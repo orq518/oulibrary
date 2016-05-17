191c8df6f1085c88a419540f79775a7690fb47a6
欧瑞强 <ouruiqiang@ucfgroup.com>
2016-05-17 14:55:09 +0800

    init

---
+++ src/main/java/cjlb/com/oulibrary/UcfInterface.java
@@ -0,0 +1,13 @@
+package cjlb.com.oulibrary;
+
+/**
+ * The author 欧瑞强 on 2016/5/17.
+ * todo
+ */
+public class UcfInterface {
+
+    public static String  getTestString(){
+        return "哈哈哈哈";
+
+    }
+}
